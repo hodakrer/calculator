cd3069cd6cabca3700c49698cfd462fa57da1f4f
choijihyuk <jihyuk0609@naver.com>
2025-08-27 15:45:49 +0900

    9단계 red,green,blue 처리

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -28,6 +28,12 @@ public class Calc {
         //green 7
         //return 80
 
+        //green 8
+        //return 60
+
+        //green 9
+        //return 20
+
         //blue1
         //blue2
         //blue3
@@ -35,31 +41,71 @@ public class Calc {
         //blue5
         //blue6
         //blue7
+        //blue8
+        //blue9
         StringTokenizer st = new StringTokenizer(mathExpression);
 
         int operandHead = INT_MEANINGLESS;
+        int operandMiddle = INT_MEANINGLESS;
         int operandTail = INT_MEANINGLESS;
-        String operator = STRING_MEANINGLESS;
 
+        String operatorHead = STRING_MEANINGLESS;
+        String operatorTail = STRING_MEANINGLESS;
         if (st.hasMoreTokens()) {
             operandHead = Integer.parseInt(st.nextToken());
         }
 
         if (st.hasMoreTokens()) {
-            operator = st.nextToken();  // 연산자
+            operatorHead = st.nextToken();  // 연산자
+        }
+
+        if (st.hasMoreTokens()) {
+            operandMiddle = Integer.parseInt(st.nextToken());
+        }
+
+        if (st.hasMoreTokens()) {
+            operatorTail = st.nextToken();
         }
 
         if (st.hasMoreTokens()) {
             operandTail = Integer.parseInt(st.nextToken());
         }
 
-        switch (operator) {
-            case "+": return operandHead + operandTail;
-            case "-": return operandHead - operandTail;
-            case "*": return operandHead * operandTail;
-            case "/": return operandHead / operandTail;
-            default: throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
+        if(operatorTail.equals(STRING_MEANINGLESS)){
+            switch (operatorHead) {
+                case "+": return operandHead + operandMiddle;
+                case "-": return operandHead - operandMiddle;
+                case "*": return operandHead * operandMiddle;
+                case "/": return operandHead / operandMiddle;
+                default: throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
+            }
+
+        }else{//two operator
+            if(operatorTail.equals("+")){
+                switch (operatorHead) {
+                    case "+": return operandHead + operandMiddle + operandTail;
+                    case "-": return operandHead - operandMiddle + operandTail;
+                    case "*": return operandHead * operandMiddle + operandTail;
+                }
+            }
+            else if(operatorTail.equals("-")){
+                switch (operatorHead) {
+                    case "+": return operandHead + operandMiddle - operandTail;
+                    case "-": return operandHead - operandMiddle - operandTail;
+                    case "*": return operandHead * operandMiddle - operandTail;
+                }
+            }
+            else if(operatorTail.equals("*")){
+                switch (operatorHead) {
+                    case "+": return operandHead + operandMiddle * operandTail;
+                    case "-": return operandHead - operandMiddle * operandTail;
+                    case "*": return operandHead * operandMiddle * operandTail;
+                }
+            }
+            else return INT_MEANINGLESS;
         }
 
+
+        return INT_MEANINGLESS;
     }
 }
