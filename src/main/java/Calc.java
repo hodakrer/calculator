900cdf58f1a961e25c62aecd939b5930b0ccb99b
choijihyuk <jihyuk0609@naver.com>
2025-08-27 15:13:41 +0900

    1단계 red, green, blue 처리.

---
+++ src/main/java/Calc.java
@@ -0,0 +1,41 @@
+import java.util.StringTokenizer;
+
+public class Calc {
+
+    static final int INT_MEANINGLESS = Integer.MIN_VALUE;
+    static final String STRING_MEANINGLESS = "";
+    static int answer = INT_MEANINGLESS;
+
+    static public int run(String mathExpression){
+        //green
+        //return 2;
+
+        //blue
+        StringTokenizer st = new StringTokenizer(mathExpression);
+
+        int operandHead = INT_MEANINGLESS;
+        int operandTail = INT_MEANINGLESS;
+        String operator = STRING_MEANINGLESS;
+
+        if (st.hasMoreTokens()) {
+            operandHead = Integer.parseInt(st.nextToken());
+        }
+
+        if (st.hasMoreTokens()) {
+            operator = st.nextToken();  // 연산자
+        }
+
+        if (st.hasMoreTokens()) {
+            operandTail = Integer.parseInt(st.nextToken());
+        }
+
+        switch (operator) {
+            case "+": return operandHead + operandTail;
+            case "-": return operandHead - operandTail;
+            case "*": return operandHead * operandTail;
+            case "/": return operandHead / operandTail;
+            default: throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
+        }
+
+    }
+}
